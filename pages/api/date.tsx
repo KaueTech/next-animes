e2f62fbe6993eae2716733578104495df4f8c96e
Kauê Dias <kauedias.dev@gmail.com>
2021-05-13 18:37:36 -0300

    add delay

--- pages/api/date.tsx
+++ pages/api/date.tsx
@@ -2,5 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate')
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  delay(5000)
+
   res.status(200).json({ date: new Date().toUTCString() })
 }
